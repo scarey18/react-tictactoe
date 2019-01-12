4fd13c35605482cfca7571b38afd4ee2510e58d3
Sean Carey <seancarey18@gmail.com>
2019-01-11 21:47:23 -0500

    Updates

---
+++ precache-manifest.96e1f278494107da7e3dfef637a555b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4cf06d2621ff54be7e0",
+    "url": "/react-tictactoe/static/js/runtime~main.b4cf06d2.js"
+  },
+  {
+    "revision": "46363aeacbad14c4fd30",
+    "url": "/react-tictactoe/static/js/main.46363aea.chunk.js"
+  },
+  {
+    "revision": "abd9e857442f04b96f9b",
+    "url": "/react-tictactoe/static/js/1.abd9e857.chunk.js"
+  },
+  {
+    "revision": "46363aeacbad14c4fd30",
+    "url": "/react-tictactoe/static/css/main.dbbc0f1a.chunk.css"
+  },
+  {
+    "revision": "747f54af9bc9e5ca2508ae94da453a92",
+    "url": "/react-tictactoe/index.html"
+  }
+];
