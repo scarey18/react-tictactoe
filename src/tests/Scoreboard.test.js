76e5d52ec9634e357c0e92b63530ec57716e65cb
Sean Carey <seancarey18@gmail.com>
2020-09-14 19:26:23 -0400

    Add tests for Scoreboard component

---
+++ src/tests/Scoreboard.test.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+
+import Scoreboard from '../components/Scoreboard';
+
+
+let container = null;
+
+beforeEach(() => {
+	container = document.createElement('div');
+	document.body.appendChild(container);
+});
+
+afterEach(() => {
+	unmountComponentAtNode(container);
+	container.remove();
+	container = null;
+});
+
+
+test("renders all props correctly", () => {
+
+	act(() => {
+		render((
+			<Scoreboard
+				message="Welcome!"
+				playerScore={0}
+				cpuScore={1}
+			/>
+		), container)
+	});
+
+	let header = container.querySelector('h1');
+	let playerScore = container.querySelector('#player-score');
+	let cpuScore = container.querySelector('#cpu-score');
+
+	expect(header.textContent).toBe('Welcome!');
+	expect(playerScore.textContent).toBe('0');
+	expect(cpuScore.textContent).toBe('1');
+
+	act(() => {
+		render((
+			<Scoreboard
+				message="You are X"
+				playerScore={1}
+				cpuScore={3}
+			/>
+		), container)
+	});
+
+	expect(header.textContent).toBe('You are X');
+	expect(playerScore.textContent).toBe('1');
+	expect(cpuScore.textContent).toBe('3');
+});
